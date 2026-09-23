8bb1702d5ec74207108f04ccd9acd1d3f58d7738
Dust North Games <joeywsanders@gmail.com>
2026-09-23 20:25:13 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 // NEON ENGLISH configuration
-// GitHub Pages can safely expose the Supabase URL + ANON key when RLS is enabled.
+// GitHub Pages can safely expose the Supabase URL + publishable/anon key when RLS is enabled.
 // NEVER place your Supabase service_role key here.
+
 window.NEON_CONFIG = {
   supabaseUrl: "https://foiscupyuiytyrmssrif.supabase.co",
   supabaseAnonKey: "sb_publishable_15gkstOk6Ctxxa1tsSxy7w_i5n26En_",
